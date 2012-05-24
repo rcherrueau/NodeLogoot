85f94152cc2a4cbe90da1964d54e44d7de413cc2
Ronan Cherrueau <ronancherrueau@gmail.com>
2012-05-24 03:22:00 +0200

    Partie motivation

---
+++ doc/byc/includes/motivations.mm
@@ -0,0 +1,22 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1337816605635" ID="ID_1155161921" MODIFIED="1337817944853" TEXT="Motivation">
+<node CREATED="1337816973886" HGAP="-11" ID="ID_889078627" MODIFIED="1337819263869" POSITION="right" TEXT="&#xc9;conomie" VSHIFT="-15">
+<node CREATED="1337817888462" HGAP="29" ID="ID_1267820950" MODIFIED="1337817937365" TEXT="Infrastructure l&#xe9;g&#xe8;re et d&#xe9;centralis&#xe9;e" VSHIFT="-8"/>
+<node CREATED="1337817337446" HGAP="27" ID="ID_1143603221" MODIFIED="1337817942103" TEXT="Ressources materielles fournies par les &#xe9;diteurs du document" VSHIFT="-4"/>
+<node CREATED="1337817453416" HGAP="25" ID="ID_603417145" MODIFIED="1337817940413" TEXT="Ressources logicelles fournies par les &#xe9;diteurs du document" VSHIFT="-5"/>
+<node CREATED="1337817634638" ID="ID_1728263224" MODIFIED="1337817644038" TEXT="Faibles investissements c&#xf4;t&#xe9; serveur"/>
+</node>
+<node CREATED="1337816990246" HGAP="9" ID="ID_1135467593" MODIFIED="1337819256837" POSITION="left" TEXT="Suret&#xe9;" VSHIFT="-7">
+<node CREATED="1337818600470" ID="ID_133575535" MODIFIED="1337818655171" TEXT="Int&#xe9;grit&#xe9; du document garantie"/>
+<node CREATED="1337818658390" ID="ID_268371801" MODIFIED="1337818675720" TEXT="Passe &#xe0; l&apos;&#xe9;chelle en nombre d&apos;&#xe9;diteurs"/>
+<node CREATED="1337818685912" ID="ID_1066404120" MODIFIED="1337818701157" TEXT="Passe &#xe0; l&apos;&#xe9;chelle en nombre d&apos;&#xe9;ditions"/>
+<node CREATED="1337818705830" ID="ID_869499492" MODIFIED="1337818725965" TEXT="Tol&#xe9;rance aux pannes"/>
+</node>
+<node CREATED="1337817065782" HGAP="-9" ID="ID_104043391" MODIFIED="1337819251861" POSITION="right" TEXT="S&#xe9;cutit&#xe9;" VSHIFT="47">
+<node CREATED="1337818317463" HGAP="18" ID="ID_1739751960" MODIFIED="1337818465962" TEXT="Confidentialit&#xe9; du document garantie" VSHIFT="-14"/>
+<node CREATED="1337818355984" HGAP="22" ID="ID_1978093155" MODIFIED="1337818370333" TEXT="R&#xe9;sistance &#xe0; la censure" VSHIFT="-10"/>
+<node CREATED="1337818377878" ID="ID_294966040" MODIFIED="1337818577707" TEXT="Int&#xe9;grit&#xe9; du document garantie"/>
+</node>
+</node>
+</map>
